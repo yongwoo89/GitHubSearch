0b7cb83ceb6d6dabe7664d14ebc2820950a6399c
yongwoo89 <wh8818@naver.com>
2022-11-04 13:37:54 +0900

    Project Initial commit

---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by yongwoo on 2022/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+        
+    }
+}
+
